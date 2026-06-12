8dd952b630924c9be7ea28bf1af96583dbb39521
dishonerd2021-spec <dishonerd2021@gmail.com>
2026-06-12 20:35:47 +0330

    Add references module __init__.py

---
+++ references/__init__.py
@@ -0,0 +1,11 @@
+"""
+References/Bibliography management module
+"""
+
+from references.bibtex_manager import BibTexManager
+from references.citation_engine import CitationEngine
+
+__all__ = [
+    'BibTexManager',
+    'CitationEngine'
+]
