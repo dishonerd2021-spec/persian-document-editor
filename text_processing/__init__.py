0ac558ed0ed7eaa1de9c675bb88768548c0e78ff
dishonerd2021-spec <dishonerd2021@gmail.com>
2026-06-12 20:35:21 +0330

    Add text processing module __init__.py

---
+++ text_processing/__init__.py
@@ -0,0 +1,11 @@
+"""
+Text processing module initialization
+"""
+
+from text_processing.persian_processor import PersianProcessor
+from text_processing.english_processor import EnglishProcessor
+
+__all__ = [
+    'PersianProcessor',
+    'EnglishProcessor'
+]
